377dd717df1ef7afce29d43ea4dc841c86d7707f
Meena3010 <54390073+Meena3010@users.noreply.github.com>
2019-10-01 15:26:13 +0530

    Add files via upload

---
+++ For ass 5/Spring Mvc/springmvcfirst/src/com/javatpoint/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.javatpoint;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController 
+{
+
+	@RequestMapping("/hello")
+	public ModelAndView helloWorld(){
+		String message="Hello Spring MVC how r u";
+		return new ModelAndView("hellopage","message",message);
+	}
+	@RequestMapping("/welcome")
+	public ModelAndView welcomeWorld(){
+		String message="Hello Welcome to Spring MVC";
+		return new ModelAndView("welcome","message",message);
+	}
+	
+	
+}
